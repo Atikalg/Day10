a26d5ff2bf7bc622cdffb0b4e1d0af1653e1804b
atikalg <who.atik@gmail.com>
2022-03-23 11:31:12 +0530

    added head or tail as winner

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,2 +1,21 @@
 #!/bin/bash -x
 echo "Welcome to flip coin simulator"
+headsWins=0
+tailsWins=0
+ifHeads=0
+ifTails=1
+counter=0
+function flipCoinSimulation()
+{
+check=$((RANDOM%2))
+counter=$(($counter+1))
+if [ $check -eq $ifHeads ]
+then
+	headsWins=$(($headsWins+1))
+elif [ $check -eq $ifTails ]
+then
+	tailsWins=$(($tailsWins+1))
+fi
+headsMargin=$(($headsWins-$tailsWins))
+tailsMargin=$(($tailsWins-$headsWins))
+}
